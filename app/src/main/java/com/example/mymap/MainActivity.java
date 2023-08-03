3c99a65dbd379bcd4a31580f12c20b972f71852f
aleena02122002 <khananna919@gmail.com>
2023-08-03 18:21:57 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mymap/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mymap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mymap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mymap = googleMap;
+        LatLng sydney = new LatLng(-34,151);
+        mymap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
+        mymap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
